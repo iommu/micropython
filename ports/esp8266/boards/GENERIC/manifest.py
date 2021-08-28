9186c5ba2cd4313f6a4fdafe4f78e0a64a30e11c
robert-hh <robert@hammelrath.com>
2021-08-28 18:39:54 +0200

    esp8266: Fix manifest.py.

--- ports/esp8266/boards/GENERIC/manifest.py
+++ ports/esp8266/boards/GENERIC/manifest.py
@@ -9,8 +9,9 @@ freeze("$(MPY_DIR)/drivers/display", "ssd1306.py")
 # Libraries from micropython-lib, include only if the library directory exists
 if os.path.isdir(convert_path("$(MPY_LIB_DIR)")):
     # umqtt
-    freeze("$(MPY_LIB_DIR)/micropython/umqtt.simple", "umqtt/simple.py")
-    freeze("$(MPY_LIB_DIR)/micropython/umqtt.robust", "umqtt/robust.py")
+    freeze("$(MPY_LIB_DIR)/umqtt.simple", "umqtt/simple.py")
+    freeze("$(MPY_LIB_DIR)/umqtt.robust", "umqtt/robust.py")
 freeze('$(MPY_DIR)/../pye', 'pye_mp.py')
 freeze('$(MPY_DIR)/../uftpd', 'uftpd.py')
+freeze('$(MPY_DIR)/../upysh', 'upysh.py')
 # freeze('$(MPY)/../uping', 'uping.py')
