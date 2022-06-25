200f43b1baef9be830463379a44fc1c75778fed8
robert-hh <robert@hammelrath.com>
2022-06-25 21:46:03 +0200

    mimxrt: Allow local write only after a soft reset.
    Since remount after MSC eject does not update the file system's data
    in memory, keep file the system readonly until after a soft reset, which
    has to be done deliberately.
    That's neither comfortable nor elegant, but safe.
    
    Disabled MSC as default option.
    Disabled MSC for MIMXRT1050_EVK deliberately.
    Enabled MSC for MIMXRT1020_EVK, Teensy 4.1 and OLIMEX_RT1010 for testing.

--- ports/mimxrt/boards/TEENSY41/mpconfigboard.h
+++ ports/mimxrt/boards/TEENSY41/mpconfigboard.h
@@ -1,6 +1,8 @@
 #define MICROPY_HW_BOARD_NAME "Teensy 4.1"
 #define MICROPY_HW_MCU_NAME   "MIMXRT1062DVJ6A"
 
+#define MICROPY_HW_USB_MSC    (1)
+
 // Teensy 4.1 has 1 board LED
 #define MICROPY_HW_LED1_PIN (pin_GPIO_B0_03)
 #define MICROPY_HW_LED_ON(pin) (mp_hal_pin_high(pin))
